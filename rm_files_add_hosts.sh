e94f7fc875e61a5d50912b92271cb822c4a3dda8
WMichau <58782510+WMichau@users.noreply.github.com>
2022-06-03 19:24:17 +0200

    Add files via upload

---
+++ rm_files_add_hosts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hosts_f=$1
+pass_f=$2
+
+readarray -t arr < $pass_f
+
+while read -r line; do
+	ssh -n ${line} "sudo su -c 'find /tmp/ -type f -iname '*zutylizuj*' -delete'"
+done < $hosts_f
+
+ssh -n repo-1.dc-2.tools.dcwp.pl "sudo su -c 'cd ~/operator/systemy/; echo "" >> ./repo.txt; printf "%s\n" "${arr[@]}" >> ./repo.txt'"
