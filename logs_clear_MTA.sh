527e0e69c7a05fd1bccb0e213893f0870d99d3ed
WMichau <58782510+WMichau@users.noreply.github.com>
2022-06-18 09:00:01 +0200

    Update logs_clear_MTA.sh

--- logs_clear_MTA.sh
+++ logs_clear_MTA.sh
@@ -4,10 +4,9 @@ do
  for i in {1..12}
  do
  	if [ "$(ssh mta-$i.dc-$dc.poczta.dcwp.pl df -h | grep var | awk '{print $5+0}')" -gt "90" ]
- 	then
+	then
  		ssh mta-$i.dc-$dc.poczta.dcwp.pl "sudo su -c '>/var/log/antyspam-json.log'"
- 
-	echo "mta-$i.dc-$dc.poczta.dcwp.pl | $(date '+%d/%m/%Y %H:%M:%S')" >> mta_log.log
- fi
+ 		echo "mta-$i.dc-$dc.poczta.dcwp.pl | $(date '+%d/%m/%Y %H:%M:%S')" >> mta_log.log
+ 	fi
  done
 done
