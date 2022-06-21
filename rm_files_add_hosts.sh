317c2188dec97cb936840bccd9e1b7065cf0c2a9
WMichau <58782510+WMichau@users.noreply.github.com>
2022-06-22 01:25:47 +0200

    Update rm_files_add_hosts.sh

--- rm_files_add_hosts.sh
+++ rm_files_add_hosts.sh
@@ -8,6 +8,6 @@ while read -r line; do
 	ssh -n ${line} "sudo su -c 'find /tmp/ -type f -iname '*zutylizuj*' -delete'"
 done < $hosts_f
 
+scp $pass_f repo-1.dc-2.tools.dcwp.pl:/tmp/
 
-#issue_to_resolve
-#ssh repo-1.dc-2.tools.dcwp.pl "sudo su -c 'cd ~/operator/systemy/; echo "" >> ./repo.txt; printf "%s\n" "${arr[@]}" >> ./repo.txt'"
+ssh -t -n repo-1.dc-2.tools.dcwp.pl "sudo -i awk 'BEGIN{print""}1' /tmp/$pass_f | sudo -i tee -a /root/operator/systemy/repo.txt"
