6a70da57b70e080794770f42bb42f76dcd32e469
WMichau <michauw@protonmail.com>
2022-07-01 03:30:55 +0200

    Update disk_localization.sh

--- disk_localization.sh
+++ disk_localization.sh
@@ -1,53 +1,45 @@
 #!/bin/bash
 
+#Usage: ssh IP/NAME "bash -s" < /path/disk_localization.sh SERIALNUMBER
 
 CONTROLLER=""
 SERIAL=$1
 
 check_controller () {
 
-	#Megacli
+	SLOT=$(sudo -S hpssacli ctrl all show config | sed -n 2p | awk '{print $6}')
+
 	if [ $(sudo -S /opt/MegaRAID/MegaCli/MegaCli64 -adpcount | grep -i controller | awk '{printf $3}' | sed 's/.$//') != "0" ]
 	then
-		CONTROLLER="MegaCli64"
-		return 0
-	fi
+                CONTROLLER="MegaCli64"
 
-	#HP
-	if sudo -S hpssacli ctrl slot=1 pd all show detail
-	then
-		CONTROLLER="HP"
-		return 0
-	fi
+        elif sudo -S hpssacli ctrl slot=$SLOT pd all show detail &>/dev/null
+        then
+                CONTROLLER="HP"
 
-	$SAS
-	if sudo -S /usr/local/bin/sas2ircu 0 DISPLAY &>/dev/null
-	then
-		CONTROLLER="SAS"
-		return 0
-	fi
+        elif sudo -S /usr/local/bin/sas2ircu 0 DISPLAY &>/dev/null
+        then
+                CONTROLLER="SAS"
 
-	echo "No hardware RAID controller"
-
+        else
+                echo "No hardware RAID controller"
+                exit 0
+        fi
 }
 
-check_controller
-echo $CONTROLLER
+get_localization () {
 
-#MegaCli64 () {
-#
-#	/opt/MegaRAID/MegaCli/MegaCli64 -PDLIst -aall;
-#
-#}
-#
-#HPSsaCli () {
-#	
-#	hpssacli ctrl slot=1 pd all show detail;
-#
-#}
-#
-#SaS () {
-#
-#	/usr/local/bin/sas2ircu 0 display
-#
-#}
+        check_controller
+        if [ $CONTROLLER == "MegaCli64" ]
+        then
+                sudo -S /opt/MegaRAID/MegaCli/MegaCli64 -PDLIst -aall | grep -i "Enclosure Device\|Slot\|Device id\|Drive's position\|$SERIAL" | grep "$SERIAL"  -B 4
+        elif [ $CONTROLLER == "HP" ]
+        then
+                sudo -S hpssacli ctrl slot=$SLOT pd all show detail | grep -i "Port\|Box\|Bay\|Model\|$SERIAL" | grep "$SERIAL" -B 3 -A 1
+        elif [ $CONTROLLER == "SAS" ]
+        then
+                sudo -S /usr/local/bin/sas2ircu 0 DISPLAY | grep -i "Enclosure\|Slot\|State\|Model\|$SERIAL" | grep "$SERIAL" -B 4
+        fi
+}
+
+get_localization
